c4ddc95196f52cbbb82f5bb8bb73c7055ccf1ffa
Harihara <hariharasethi89@gmail.com>
2026-01-25 21:54:36 +0530

    feat: add source command flag

--- main.go
+++ main.go
@@ -1,36 +1,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log/slog"
+	"os"
+	"slices"
 	"strings"
 
 	"github.com/harihara713/tech-signals/article"
 	"github.com/harihara713/tech-signals/source"
 )
 
+var sourceNames = []string{"meta", "google", "amazon", "github", "stripe"}
+
 func main() {
-	sources := []source.Source{
-		source.Meta{},
-		source.Google{},
-		source.Amazon{},
-		source.Stripe{},
-		source.Github{},
-	}
+	var (
+		selectedSource string
+		limit          int
+		saveFilename   string
+	)
 
-	artStore := article.NewArticleStore()
+	flag.StringVar(&selectedSource, "source", "", "source name (Meta, Google, Amazon, Github, Stripe)")
+	flag.IntVar(&limit, "limit", 0, "max number of blogs to fetch (0 = all)")
+	flag.StringVar(&saveFilename, "save", "", "output filename (json)")
 
-	for _, s := range sources {
-		if err := s.Fetch(artStore); err != nil {
-			slog.Error("Error fetching articles", "err", err)
+	flag.Parse()
+
+	if selectedSource != "" {
+		selectedSource = strings.ToLower(selectedSource)
+		var validSource bool
+		if slices.Contains(sourceNames, selectedSource) {
+			validSource = true
+		}
+
+		if !validSource {
+			fmt.Fprintf(os.Stderr, "%s: invalid source name,\nPlease provide valid source name(meta, google, amazon, github, stripe)", selectedSource)
+			os.Exit(1)
+		}
+
+		var articles *article.ArticleStore
+		// fetch all the blogs for a particular source
+		switch selectedSource {
+		case "meta":
+			source.Meta{}.Fetch(articles)
+		case "google":
+			source.Google{}.Fetch(articles)
+		case "amazon":
+			source.Amazon{}.Fetch(articles)
+		case "github":
+			source.Github{}.Fetch(articles)
+		case "stripe":
+			source.Stripe{}.Fetch(articles)
+		}
+
+		fmt.Printf("\nTotal %d articles fetched\n\n", len(*articles))
+		for _, art := range *articles {
+			fmt.Printf("[%s]\nTitle: %s\nUrl: %s\nPublished At: %v\nAuthor: %s\nSummary: %s\nTags: %s\n\n", art.Source, art.Title,
+				art.URL, art.PublishedAt, art.Author, art.Summary, strings.Join(art.Tags, ", "))
 		}
 	}
 
-	// show
-	fmt.Println("\n" + strings.Repeat("=", 100) + "\n")
-	fmt.Printf("Total %d articles found\n\n", len(*artStore))
-
-	for _, a := range *artStore {
-		fmt.Printf("%+v\n", a.Title)
-	}
 }
